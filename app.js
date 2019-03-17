6f0657a8c2fd3399e7fcb960a1adcbdac1bf6a7d
Joshua <jsc91@live.co.uk>
2019-03-17 09:26:00 +0000

    Add chance package and title generator

--- app.js
+++ app.js
@@ -24,6 +24,30 @@ const startingItems = [
   "Book Of Revelations"
 ];
 
+const colours = [
+  "Red",
+  "Blue",
+  "Indigo",
+  "Brown",
+  "Orange",
+  "Yellow",
+  "Violet",
+  "Green",
+  "Olive"
+];
+
+const things = [
+  "Bathtub",
+  "Sink",
+  "Table",
+  "Fish",
+  "Dog",
+  "Cat",
+  "Beaver",
+  "Pistol",
+  "Pen"
+];
+
 // Get a random element from the array based on the seed provided.
 function getRandomWithSeed(array, seed, quantity = 1) {
   const random = createRandom(seed);
@@ -47,11 +71,15 @@ today.setUTCHours(0, 0, 0, 0);
 
 const seed = today.getTime();
 
+const playableCharacter = getRandomWithSeed(characters, seed);
+
+const title = `${getRandomWithSeed(colours, seed)}
+  ${playableCharacter}
+  ${getRandomWithSeed(things, seed)}`;
+
+document.getElementById("title").textContent = title;
 document.getElementById("seed").textContent = seed;
-document.getElementById("character").textContent = getRandomWithSeed(
-  characters,
-  seed
-);
+document.getElementById("character").textContent = playableCharacter;
 document.getElementById("startingItems").textContent = getRandomWithSeed(
   startingItems,
   seed,
