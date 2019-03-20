c60b16a952bdb67aee9d0408bd72b771f50255be
Joshua <jsc91@live.co.uk>
2019-03-20 20:10:35 +0000

    Add js folder

--- js/app.js
+++ js/app.js
@@ -1,24 +1,8 @@
-import createRandom from "random-seed";
+import { getRandomWithSeed } from "./random";
 const items = require("./json/items.json");
 const colours = require("./json/colours.json");
 const adjectives = require("./json/adjectives.json");
 const characters = require("./json/characters.json");
-
-// Get a random element from the array based on the seed provided.
-function getRandomWithSeed(array, seed, quantity = 1) {
-  const random = createRandom(seed);
-  const remaining = array.slice();
-  const picked = [];
-
-  for (let i = 0; i < quantity; i++) {
-    const index = random(remaining.length);
-
-    picked.push(remaining[index]);
-    remaining.splice(index, 1);
-  }
-
-  return picked;
-}
 
 const today = new Date();
 
---
+++ js/random.js
@@ -0,0 +1,16 @@
+import createRandom from "random-seed";
+// Get a random element from the array based on the seed provided.
+export function getRandomWithSeed(array, seed, quantity = 1) {
+  const random = createRandom(seed);
+  const remaining = array.slice();
+  const picked = [];
+
+  for (let i = 0; i < quantity; i++) {
+    const index = random(remaining.length);
+
+    picked.push(remaining[index]);
+    remaining.splice(index, 1);
+  }
+
+  return picked;
+}
