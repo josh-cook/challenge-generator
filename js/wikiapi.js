6d3e9d9e4bcb2f0fdf3f68ac1912cdfcb9e79ffe
NashPL <nash25pl@gmail.com>
2019-07-25 02:37:56 +0100

    Added bit of comments.

--- js/wikiapi.js
+++ js/wikiapi.js
@@ -1,17 +1,20 @@
 import regeneratorRuntime from "regenerator-runtime";
 
+//WikiAPI class for the fetching wiki content. 
 export default class WikiApi {
     constructor() {
         this.url = 'https://bindingofisaacrebirth.gamepedia.com';
         this.request = '/api.php?origin=*&action=query&format=json&prop=info%7Cpageimages%7Cextracts&titles=';
     }
 
+    //Simple fetch by name function
     async getPageByName(name) {
         let result = await fetch(this.url + this.request + encodeURI(name));
         return await result.json();
 
     }
 
+    //Parse the results back and get only needed details. 
     async getPageInfoResult(name) {
         const page = await this.getPageByName(name);
         let res = {};
