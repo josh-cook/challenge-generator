d430a466fb2a3e76e13367e3a17e1062e634e532
Joshua <jsc91@live.co.uk>
2019-10-14 18:45:07 +0100

    Add react

---
+++ src/ChallengeGenerator.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ChallengeGenerator extends Component {
+  render() {
+    return (
+      <div>
+        Challenge Generator
+      </div>
+    )
+  }
+}
+
+export default ChallengeGenerator
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import ChallengeGenerator from './ChallengeGenerator';
+
+ReactDOM.render(<ChallengeGenerator />, document.getElementById("wrapper"));
